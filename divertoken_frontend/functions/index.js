88c9a5f730433589b076413047364d1ab4e148fa
alias-r <song@divertise.asia>
2021-12-09 09:59:34 +0700

    implement verifyTask cloudfunction basic

--- divertoken_frontend/functions/index.js
+++ divertoken_frontend/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +8,30 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.verifyTask = functions.https.onRequest((req, res) => {
+  functions.logger.info("Task verified!", { structuredData: true });
+  //   response.send("Task verified!");
+  const taskId = req.body.taskId;
+  const userId = req.body.userId;
+  return admin.database().ref("tasks/" + taskId).once("value", (snapshot) => {
+    const task = snapshot.val();
+    res.send(`
+          <!doctype html>
+          <html>
+              <head>
+                  <title>${task.name}</title>
+              </head>
+              <body>
+                  <h1>Title ${task.name} is verified by ${userId}</h1>
+              </body>
+          </html>`
+    );
+  });
+});
+
+exports.claimTask = functions.https.onRequest((request, response) => {
+  functions.logger.info("Task claim!", {structuredData: true});
+  response.send("Task claim!");
+});
+
